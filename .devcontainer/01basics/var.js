5bc999c68a1228a00d83e83b60ffa5fc348140dd
dhairya-22 <123473774+dhairya-22@users.noreply.github.com>
2023-12-20 14:07:41 +0000

    basic var testing

---
+++ .devcontainer/01basics/var.js
@@ -0,0 +1,12 @@
+const accountId=14453
+let accountEmail="hitesh@google.com"
+var accountpassword="12345"
+accountcity ="jaipur" 
+
+// accountId=2
+
+
+accountEmail="hc@hcmoldnc"
+
+console.log(accountId)
+console.table([accountEmail,accountId,accountpassword])
