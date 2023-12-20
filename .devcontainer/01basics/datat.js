e986700b35647889db62e498e2a1065a10c987a4
dhairya-22 <123473774+dhairya-22@users.noreply.github.com>
2023-12-20 14:40:15 +0000

    datatypes

---
+++ .devcontainer/01basics/datat.js
@@ -0,0 +1,7 @@
+"use strict";//use all code after this as new js code
+//alert(3+3) we are using nodejs
+let name="shaurya"
+let age=3
+let idiot=true
+let state=null
+console.log(typeof null)//always shows null as object data 
